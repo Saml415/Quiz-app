d180c41a47de6c1dfbc67b4badc683dc1c2bfd96
Sam Lloyd <bobdoe784@gmail.com>
2021-06-18 20:40:21 -0700

    question variable with options and answers have been made

---
+++ assets/script.js
@@ -0,0 +1,53 @@
+const myQuestions = [
+  {
+    question: "Who won the 1992 NL Cy Young Award?",
+    answers: {
+      a: "Greg Maddux",
+      b: "David Cone",
+      c: "Dennis Eckersley",
+      d: "John Smoltz",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "When did Disneyland first open?",
+    answers: {
+      a: "1953",
+      b: "1955",
+      c: "1976",
+      d: "1922",
+    },
+    correctAnswer: "b",
+  },
+
+  {
+    question: "What Country do Legos originate from?",
+    answers: {
+      a: "Japan",
+      b: "The Netherlands",
+      c: "Russia",
+      d: "Denmark",
+    },
+    correctAnswer: "d",
+  },
+  {
+    question: "What year did the last astronaut land on the moon?",
+    answers: {
+      a: "1983",
+      b: "1969",
+      c: "1972",
+      d: "1997",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Which US city has NOT hosted the Olympic games?",
+    answers: {
+      a: "Atlanta",
+      b: "New York City",
+      c: "Los Angeles",
+      d: "St.Louis",
+    },
+    correctAnswer: "b",
+  },
+];
